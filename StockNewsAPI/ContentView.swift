67f084f21e06b2ae09587ac3e83c377e71cf182f
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-03-22 09:27:07 +0000

    Initial Commit

---
+++ StockNewsAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockNewsAPI
+//
+//  Created by Luk'yanov, Mikhail (JMOB) on 22/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StockNewsAPI/StockNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockNewsAPIApp.swift
+//  StockNewsAPI
+//
+//  Created by Luk'yanov, Mikhail (JMOB) on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StockNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
